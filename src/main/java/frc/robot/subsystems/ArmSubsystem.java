c21c8acbf90f5068d2a7d65bc16cb4b804a7cfe8
Asagarwala2716 <asagarwala.27@presentationhs.org>
2025-06-27 15:52:32 -0700

    starting to modify code

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -13,41 +13,42 @@ import frc.robot.Constants.ArmConstants;
 public class ArmSubsystem extends SubsystemBase {
 
     private final SparkMax armMotor;
-    
+
     /**
      * This subsytem that controls the arm.
      */
-    public ArmSubsystem () {
+    public ArmSubsystem() {
 
-    // Set up the arm motor as a brushed motor
-    armMotor = new SparkMax(ArmConstants.ARM_MOTOR_ID, MotorType.kBrushed);
+        // Set up the arm motor as a brushed motor
+        armMotor = new SparkMax(ArmConstants.ARM_MOTOR_ID, MotorType.kBrushed);
 
-    // Set can timeout. Because this project only sets parameters once on
-    // construction, the timeout can be long without blocking robot operation. Code
-    // which sets or gets parameters during operation may need a shorter timeout.
-    armMotor.setCANTimeout(250);
+        // Set can timeout. Because this project only sets parameters once on
+        // construction, the timeout can be long without blocking robot operation. Code
+        // which sets or gets parameters during operation may need a shorter timeout.
+        armMotor.setCANTimeout(250);
 
-    // Create and apply configuration for arm motor. Voltage compensation helps
-    // the arm behave the same as the battery
-    // voltage dips. The current limit helps prevent breaker trips or burning out
-    // the motor in the event the arm stalls.
-    SparkMaxConfig armConfig = new SparkMaxConfig();
-    armConfig.voltageCompensation(10);
-    armConfig.smartCurrentLimit(ArmConstants.ARM_MOTOR_CURRENT_LIMIT);
-    armConfig.idleMode(IdleMode.kBrake);
-    armMotor.configure(armConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        // Create and apply configuration for arm motor. Voltage compensation helps
+        // the arm behave the same as the battery
+        // voltage dips. The current limit helps prevent breaker trips or burning out
+        // the motor in the event the arm stalls.
+        SparkMaxConfig armConfig = new SparkMaxConfig();
+        armConfig.voltageCompensation(10);
+        armConfig.smartCurrentLimit(ArmConstants.ARM_MOTOR_CURRENT_LIMIT);
+        armConfig.idleMode(IdleMode.kBrake);
+        armMotor.configure(armConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     }
 
     @Override
     public void periodic() {
     }
-    /** 
+
+    /**
      * This is a method that makes the arm move at your desired speed
-     *  Positive values make it spin forward and negative values spin it in reverse
+     * Positive values make it spin forward and negative values spin it in reverse
      * 
      * @param speed motor speed from -1.0 to 1, with 0 stopping it
      */
-    public void runArm(double speed){
+    public void runArm(double speed) {
         armMotor.set(speed);
     }
 }
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -9,40 +9,113 @@ import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
 
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.CANrangeConfiguration;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.ReverseLimitSourceValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
+import com.ctre.phoenix6.signals.StaticFeedforwardSignValue;
+import com.ctre.phoenix6.signals.UpdateModeValue;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
 
 public class DriveSubsystem extends SubsystemBase {
-  private final SparkMax leftLeader;
-  private final SparkMax leftFollower;
-  private final SparkMax rightLeader;
-  private final SparkMax rightFollower;
+  // private final SparkMax leftLeader;
+  // private final SparkMax leftFollower;
+  // private final SparkMax rightLeader;
+  // private final SparkMax rightFollower;
+  private final WPI_TalonSRX leftLeaderSRX;
+  private final WPI_TalonSRX leftFollowerSRX;
+  private final WPI_TalonSRX rightLeaderSRX;
+  private final WPI_TalonSRX rightFollowerSRX;
 
   private final DifferentialDrive drive;
 
   /**
    * The subsystem used to drive the robot.
    */
+  
   public DriveSubsystem() {
+    
+
     // create brushed motors for drive
-    leftLeader = new SparkMax(DriveConstants.LEFT_LEADER_ID, MotorType.kBrushed);
-    leftFollower = new SparkMax(DriveConstants.LEFT_FOLLOWER_ID, MotorType.kBrushed);
-    rightLeader = new SparkMax(DriveConstants.RIGHT_LEADER_ID, MotorType.kBrushed);
-    rightFollower = new SparkMax(DriveConstants.RIGHT_FOLLOWER_ID, MotorType.kBrushed);
+    // leftLeader = new SparkMax(DriveConstants.LEFT_LEADER_ID, MotorType.kBrushed);
+    // leftFollower = new SparkMax(DriveConstants.LEFT_FOLLOWER_ID, MotorType.kBrushed);
+    // rightLeader = new SparkMax(DriveConstants.RIGHT_LEADER_ID, MotorType.kBrushed);
+    // rightFollower = new SparkMax(DriveConstants.RIGHT_FOLLOWER_ID, MotorType.kBrushed);
+
+    leftLeaderSRX = new WPI_TalonSRX(1);
+    leftFollowerSRX = new WPI_TalonSRX(2);
+    rightLeaderSRX = new WPI_TalonSRX(3);
+    rightFollowerSRX = new WPI_TalonSRX(4);
+
+    drive = new DifferentialDrive(leftLeaderSRX, rightLeaderSRX);
+
+    TalonSRXConfiguration DriveSRXConfig(boolean inverted, double min, double max)
+    {
+      TalonSRXConfiguration DriveConfig = new TalonSRXConfiguration( );
+
+      DriveConfig.SupplyCurrentLimit = 45.0;           // Amps
+      DriveConfig.SupplyCurrentLowerLimit = 30.0;      // Amps 
+      DriveConfig.SupplyCurrentLowerTime = 0.080;      // Seconds
+      DriveConfig.SupplyCurrentLimitEnable = true;
+
+      DriveConfig.StatorCurrentLimit = 120.0;          // Amps
+      DriveConfig.StatorCurrentLimitEnable = true;
+
+    // Feedback settings
+    // elevatorConfig.Feedback.*
+
+    // Hardware limit switches - NONE
+    // elevatorConfig.HardwareLimitSwitch.*
+
+    // Motion Magic settings
+      DriveConfig.MotionMagicCruiseVelocity = 82.17;     // Rotations / second
+      DriveConfig.MotionMagicAcceleration = 300.0;       // Rotations / second ^ 2
+      DriveConfig.MotionMagicJerk = 3200;                // Rotations / second ^ 3
+
+      DriveConfig.DutyCycleNeutralDeadband = 0.001;      // Percentage
+      DriveConfig.Inverted = ((inverted) ? InvertedValue.Clockwise_Positive : InvertedValue.CounterClockwise_Positive);
+      DriveConfig.NeutralMode = NeutralModeValue.Brake;
+      
+      elevatorConfig.SoftwareLimitSwitch.ReverseSoftLimitThreshold min; // Rotations
+      elevatorConfig.SoftwareLimitSwitch.ReverseSoftLimitEnable True;
+      elevatorConfig.SoftwareLimitSwitch.ForwardSoftLimitThreshold max; // Rotations
+      elevatorConfig.SoftwareLimitSwitch.ForwardSoftLimitEnable True;
+      return DriveConfig;
+    }
 
     // set up differential drive class
-    drive = new DifferentialDrive(leftLeader, rightLeader);
+    //drive = new DifferentialDrive(leftLeader, rightLeader);
 
+    cfg = CTREConfigs6.driveFXConfig(true, min, max);
+    boolean leftValid = PhoenixUtil6.getInstance( ).TalonSRXInitialize6(leftLeaderSRX, kSubsystemName + "Left", cfg);
+    cfg = CTREConfigs6.elevatorFXConfig(false, min, max);
+    boolean rightValid = PhoenixUtil6.getInstance( ).TalonSRXInitialize6(rightLeaderSRX, kSubsystemName + "Right", cfg);
+    m_motorsValid = leftValid && rightValid;
+    
     // Set CAN timeout. Because this project only sets parameters once on
     // construction, the timeout can be long without blocking robot operation. Code
     // which sets or gets parameters during operation may need a shorter timeout.
-    leftLeader.setCANTimeout(250);
-    rightLeader.setCANTimeout(250);
-    leftFollower.setCANTimeout(250);
-    rightFollower.setCANTimeout(250);
+   
+    // leftLeader.setCANTimeout(250);
+    // rightLeader.setCANTimeout(250);
+    // leftFollower.setCANTimeout(250);
+    // rightFollower.setCANTimeout(250);
 
     // Create the configuration to apply to motors. Voltage compensation
     // helps the robot perform more similarly on different
@@ -56,48 +129,57 @@ public class DriveSubsystem extends SubsystemBase {
     // Set configuration to follow leader and then apply it to corresponding
     // follower. Resetting in case a new controller is swapped
     // in and persisting in case of a controller reset due to breaker trip
-    config.follow(leftLeader);
-    leftFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    config.follow(rightLeader);
-    rightFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    // config.follow(leftLeader);
+    // leftFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    // config.follow(rightLeader);
+    // rightFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    leftFollowerSRX.follow(leftLeaderSRX);
+    rightFollowerSRX.follow(rightLeaderSRX);
 
     // Remove following, then apply config to right leader
     config.disableFollowerMode();
-    rightLeader.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    // rightLeader.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    rightLeaderSRX.configure(config, ResetMode.kResetSafeParameters);
+    
     // Set conifg to inverted and then apply to left leader. Set Left side inverted
     // so that postive values drive both sides forward
-    config.inverted(true);
-    leftLeader.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    leftMotorGroup.inverted(true);
+    //leftLeader.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    leftLeaderSRX.configure(config, ResetMode.kResetSafeParameters);
   }
 
   @Override
   public void periodic() {
   }
+
   /**
-   *  Use this to control your drive train, with one axis of the controller moving the robot
-   *  forwards and backwards with the other axis turning the robot.
+   * Use this to control your drive train, with one axis of the controller moving
+   * the robot
+   * forwards and backwards with the other axis turning the robot.
    * 
-   *  Additionally if squared is true, it will square your controller inputs,
-   *  for instance pushing forwards on the control stick will yield
-   *  (0.5 * 0.5) = .25 or 25% power to the drivetrain.
+   * Additionally if squared is true, it will square your controller inputs,
+   * for instance pushing forwards on the control stick will yield
+   * (0.5 * 0.5) = .25 or 25% power to the drivetrain.
    * 
-   * @param xSpeed the speed forwards to back
+   * @param xSpeed    the speed forwards to back
    * @param zRotation the speed to turn at
-   * @param squared do you square the inputs from the controller
+   * @param squared   do you square the inputs from the controller
    */
   public void driveArcade(double xSpeed, double zRotation, boolean squared) {
     drive.arcadeDrive(xSpeed, zRotation, squared);
   }
 
   /**
-   * Use this to drive the robot, with one stick controlling one 
+   * Use this to drive the robot, with one stick controlling one
    * side of the drivetrain and the other stick controlling the other.
    * 
-   * @param leftSpeed speed to drive the left side of the robot at
+   * @param leftSpeed  speed to drive the left side of the robot at
    * @param rightSpeed speed to drive the right side of the robot at
-   * @param squared do you square the inputs from the controller 
+   * @param squared    do you square the inputs from the controller
    */
-  public void driveTank(double leftSpeed, double rightSpeed, boolean squared){
+  public void driveTank(double leftSpeed, double rightSpeed, boolean squared) {
     drive.tankDrive(leftSpeed, rightSpeed, squared);
   }
 }
