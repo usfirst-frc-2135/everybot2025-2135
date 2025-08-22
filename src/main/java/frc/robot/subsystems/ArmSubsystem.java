88e468ccaf52db21937c5868d90e1f9fd00f9e9d
Asagarwala2716 <asagarwala.27@presentationhs.org>
2025-08-21 17:08:58 -0700

    inverted needed motors

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -9,37 +9,43 @@ import frc.robot.Constants.OperatorConstants;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ArmSubsystem extends SubsystemBase {
+public class ArmSubsystem extends SubsystemBase
+{
 
     private final WPI_TalonSRX armMotor;
 
-    TalonSRXConfiguration ArmSRXConfig() {
-        TalonSRXConfiguration armConfig = new TalonSRXConfiguration();
+    TalonSRXConfiguration ArmSRXConfig( )
+    {
+        TalonSRXConfiguration armConfig = new TalonSRXConfiguration( );
         return armConfig;
     }
 
     /**
      * This subsytem that controls the arm.
      */
-    public ArmSubsystem() {
+    public ArmSubsystem( )
+    {
         // Set up the arm motor as a brushed motor
         armMotor = new WPI_TalonSRX(ArmConstants.ARM_MOTOR_ID);
+        armMotor.setInverted(true);
 
-        armMotor.configAllSettings(ArmSRXConfig());
+        armMotor.configAllSettings(ArmSRXConfig( ));
 
     }
 
     @Override
-    public void periodic() {
-    }
+    public void periodic( )
+    {}
 
     /**
      * This is a method that makes the arm move at your desired speed
      * Positive values make it spin forward and negative values spin it in reverse
      * 
-     * @param speed motor speed from -1.0 to 1, with 0 stopping it
+     * @param speed
+     *            motor speed from -1.0 to 1, with 0 stopping it
      */
-    public void runArm(double speed) {
+    public void runArm(double speed)
+    {
         armMotor.set(speed);
     }
 }
--- src/main/java/frc/robot/subsystems/RollerSubsystem.java
+++ src/main/java/frc/robot/subsystems/RollerSubsystem.java
@@ -7,36 +7,42 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.Constants.RollerConstants;
 
-public class RollerSubsystem extends SubsystemBase {
+public class RollerSubsystem extends SubsystemBase
+{
 
     private final WPI_TalonSRX rollerMotor;
 
-    TalonSRXConfiguration RollerSRXConfig() {
-        TalonSRXConfiguration rollerConfig = new TalonSRXConfiguration();
+    TalonSRXConfiguration RollerSRXConfig( )
+    {
+        TalonSRXConfiguration rollerConfig = new TalonSRXConfiguration( );
         return rollerConfig;
     }
 
     /**
      * This subsytem that controls the roller.
      */
-    public RollerSubsystem() {
+    public RollerSubsystem( )
+    {
         // Set up the roller motor as a brushed motor
         rollerMotor = new WPI_TalonSRX(RollerConstants.ROLLER_MOTOR_ID);
+        rollerMotor.setInverted(true);
 
-        rollerMotor.configAllSettings(RollerSRXConfig());
+        rollerMotor.configAllSettings(RollerSRXConfig( ));
     }
 
     @Override
-    public void periodic() {
-    }
+    public void periodic( )
+    {}
 
     /**
      * This is a method that makes the roller spin to your desired speed.
      * Positive values make it spin forward and negative values spin it in reverse.
      * 
-     * @param speedmotor speed from -1.0 to 1, with 0 stopping it
+     * @param speedmotor
+     *            speed from -1.0 to 1, with 0 stopping it
      */
-    public void runRoller(double speed) {
+    public void runRoller(double speed)
+    {
         rollerMotor.set(speed);
     }
 }
